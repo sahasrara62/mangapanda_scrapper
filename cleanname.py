8ced80f7c0de77e33aa0d7e61f59a1d448203d9b
prashant rana <sahasrara62@gmail.com>
2019-06-19 19:22:12 +0530

    make file name compatible name to file
    - provide a clean name to the file 
    - remove reserved named from original name and then make it compatible to windows

---
+++ cleanname.py
@@ -0,0 +1,21 @@
+import unicodedata
+import string
+
+valid_filename_chars = "-_.() %s%s" % (string.ascii_letters, string.digits)
+char_limit = 255
+
+
+def clean_filename(filename, whitelist=valid_filename_chars, replace='_'):
+    # replace spaces
+    for r in replace:
+        filename = filename.replace(r, '_')
+
+    # keep only valid ascii chars
+    cleaned_filename = unicodedata.normalize('NFKD', filename).encode('ASCII', 'ignore').decode()
+
+    # keep only whitelisted chars
+    cleaned_filename = ''.join(c for c in cleaned_filename if c in whitelist)
+    if len(cleaned_filename) > char_limit:
+        print(
+            "Warning, filename truncated because it was over {}. Filenames may no longer be unique".format(char_limit))
+    return cleaned_filename[:char_limit]
